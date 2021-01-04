c1a808b25785cb7bb532c1e9af1b81af31e0ae9d
sudhira.s <leenakarthikeyan22@gmail.com>
2021-01-04 23:25:33 +0530

    Simplest implementation 2

--- src/test/java/simplest/possible/implementation/PrimeFactorsTest.java
+++ src/test/java/simplest/possible/implementation/PrimeFactorsTest.java
@@ -10,11 +10,11 @@ import static java.util.Collections.emptyList;
 public class PrimeFactorsTest {
     @Test
     void thereAreNoPrimeFactorsFor1() {
-        Assertions.assertEquals(emptyList(), new PrimeFactors().of(1));
+        Assertions.assertEquals(emptyList(), new PrimeFactors().checkFactors(1));
     }
 
     @Test
     void primeFactorOf2Is2Itself() {
-        Assertions.assertEquals(List.of(2), new PrimeFactors().of(2));
+        Assertions.assertEquals(List.of(2), new PrimeFactors().checkFactors(2));
     }
 }
